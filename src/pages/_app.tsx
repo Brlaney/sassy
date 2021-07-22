d9cc3ed5ad0a5549e7fe5dea5291ccd996553ca5
brlaney <brlaney@outlook.com>
2021-07-22 12:33:47 -0500

    Pushing setup changes need to do index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import * as React from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
+import '@/styles/globals.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+export default function MyApp (props: AppProps) {
+  const { Component, pageProps } = props
+  const router = useRouter()
+
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='keywords' content='Scss, sass, next.js, react' />
+        <meta name='description' content='A Next.js app with Sass' />
+        <title>Sassy | Next</title>
+      </Head>
+      <Component {...pageProps} key={router.route} />
+    </>
+  )
 }
-export default MyApp
---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+
+export default class CustomDocument extends Document {
+  render () {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <Main />
+        </body>
+        <NextScript />
+      </Html>
+    )
+  }
+}
