f521d44e6d4060208180a89b77dfe817802eb69a
brlaney42 <brlaney@outlook.com>
2021-07-22 22:43:13 -0500

    Pushing the changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,26 @@ import styles from '@/styles/pages/Home.module.scss'
 
 
 export default function Home() {
+  const shapes = [
+    { id: 1, shape: 'va' },
+    { id: 2, shape: 'vb' },
+    { id: 3, shape: 'vc' },
+    { id: 4, shape: 'vd' },
+    { id: 5, shape: 've' },
+    { id: 6, shape: 'va' },
+    { id: 7, shape: 'vb' },
+    { id: 8, shape: 'vc' },
+    { id: 9, shape: 'vd' },
+    { id: 10, shape: 've' },
+    { id: 11, shape: 'va' }
+  ]
+
   return (
     <div className={styles.container}>
-      <h1>Add content yo!</h1>
+      <div />
+        {shapes.map(item => (
+          <div key={item.id} />
+        ))}
     </div>
   )
 }
